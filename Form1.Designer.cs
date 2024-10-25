a6c8e1db1fad7489dfbff859687b7b51050d19ce
du-aa <92159072+du-aa@users.noreply.github.com>
2024-10-25 02:00:20 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace labmid_CCtask2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Compile = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(247, 63);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(100, 20);
+            this.Input.TabIndex = 0;
+            // 
+            // Compile
+            // 
+            this.Compile.Location = new System.Drawing.Point(257, 101);
+            this.Compile.Name = "Compile";
+            this.Compile.Size = new System.Drawing.Size(75, 23);
+            this.Compile.TabIndex = 2;
+            this.Compile.Text = "Generate";
+            this.Compile.UseVisualStyleBackColor = true;
+            this.Compile.Click += new System.EventHandler(this.Compile_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(79, 142);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(462, 284);
+            this.Output.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Compile);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button Compile;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labmid_CCtask2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Compile_Click(object sender, EventArgs e)
+        {
+            String Initial_State = "Goal";
+            String Final_State = "turn";
+            var dict = new Dictionary<string, string>()
+            {
+                { "Goal", "CMD Goal | CMD" },
+                { "CMD", "start | stop | Action" },
+                { "Action", "accelerate | brake | turn" },
+                { "turn", "right" }  
+            };
+            Output.Clear();  
+            List<string> terminals = new List<string>(Input.Text.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries));
+
+            Output.AppendText("Initial CFG:\r\n");
+            PrintCFG(dict);
+
+            if (!dict["turn"].Contains("left") && terminals.Contains("left"))
+            {
+                Output.AppendText("\r\nInvalid command detected: 'Left' is not in the current CFG for turn.\r\n");
+                Output.AppendText("Generating updated CFG to resolve the issue...\r\n\r\n");
+
+                // Update the CFG to include 'left'
+                dict["turn"] = "left | right";
+            }
+
+            Output.AppendText("Corrected CFG:\r\n");
+            PrintCFG(dict);
+        }
+
+        private void PrintCFG(Dictionary<string, string> cfg)
+        {
+            foreach (var rule in cfg)
+            {
+                Output.AppendText($"{rule.Key} → {rule.Value}\r\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
